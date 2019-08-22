f15edc7c8aaab9fce1979b491373194de1d62d6a
Dallas James <dallasjames42@gmail.com>
2019-08-21 18:48:43 -0600

    data.js done

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -5,7 +5,7 @@ const NumberButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className="num-btn">{num}</button>
+      <button>{num}</button>
     </>
   );
 };
